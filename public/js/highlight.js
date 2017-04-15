bcf98fafbf2572f9a1811ff54442639845a1da7a
Karl <Kouralr@gmail.com>
2017-04-15 14:33:07 -0700

    Fixed bug with no unique ai suggestions

--- public/js/highlight.js
+++ public/js/highlight.js
@@ -1,3 +1,75 @@
+//
+// AI CREATED ANNOTATIONS
+//
+var elems = document.getElementsByClassName("aiHighlight");
+for (var i = 0; i < elems.length; i++) {
+    // Doing a function makes each one unique
+    aiCreation();
+}
+
+function aiCreation() {
+    var comment = document.createElement("div");
+
+    // Styles
+    comment.style.backgroundColor = "#f4f4f4";
+    comment.style.width = "20vw";
+    comment.innerHTML = "<b>Ernest.ai suggested this annotation for you! </b>";
+    comment.id = "comment" + count;
+    comment.style.display = "none";
+    comment.contentEditable = "false";
+    comment.className = "annotationedNotes";
+    comment.style.padding = "15px";
+    comment.style.marginRight = "30px";
+    comment.style.minHeight = "250px"
+    // Double click editing
+    comment.ondblclick = function () {
+        this.contentEditable = true;
+
+        //Some sort of thing to know hwat you are editing...
+        // span.style.fontWeight = "bold";
+    }
+    comment.onblur = function () {
+        this.contentEditable = false;
+    }
+    elems[i].addEventListener("mouseover", function () {
+
+        this.style.backgroundColor = "#bcd1ff";
+    });
+
+    elems[i].addEventListener("mouseleave", function () {
+        this.style.backgroundColor = "#e2ebff";
+    });
+
+    document.getElementById("comments").appendChild(comment);
+
+    // Event to hide all other notes after you click this one
+    elems[i].addEventListener("click", function () {
+        if (comment.style.display == "none") {
+            // Hide all other elements
+            var d = document.getElementsByClassName("annotationedNotes");
+            for (var i = 0; i < d.length; i++) {
+                d[i].style.display = "none";
+            }
+            comment.style.display = "block";
+        } else {
+            comment.style.display = "none";
+        }
+    });
+}
+
+// HIDE ALL ANNOTATIONED NOTES
+var elems = document.getElementsByClassName("annotationedNotes");
+    for (var i = 0; i < elems.length; i++) {
+        elems[i].style.display = "none";
+    }
+
+//
+//  END OF AI CREATED ANNOTATIONS
+//
+
+
+
+
 var isMouseDown = false;
 var isMouseMove = false;
 var book = document.getElementById("book")
@@ -7,7 +79,7 @@ document.onmousedown = function () {
 };
 document.onmouseup = function () {
     if (isMouseMove == true) {
-        highlight("#deefdc"); //#af9
+        highlight("#dee"); //#af9
     }
     isMouseMove = false;
     isMouseDown = false
@@ -29,9 +101,8 @@ function highlight(color) {
     var comment = document.createElement("div");
 
     // Styles
-    // comment.style.backgroundColor = "rgba(24, 163, 52, .7)";
     comment.style.backgroundColor = "#f4f4f4";
-    comment.style.width = "22vw";
+    comment.style.width = "20vw";
     comment.innerHTML = "Write your annotation here.";
     comment.id = "comment" + count;
     comment.style.display = "hide";
@@ -67,8 +138,6 @@ function highlight(color) {
         } else {
             comment.style.display = "none";
         }
-
-        console.log("OK");
     });
 
     span.addEventListener("mouseover", function () {
@@ -94,6 +163,7 @@ function highlight(color) {
     document.getElementById("comments").appendChild(comment);
     count += 1;
 
+    // Hide all other annotations
     var elems = document.getElementsByClassName("annotationedNotes");
     for (var i = 0; i < elems.length; i++) {
         elems[i].style.display = "none";
@@ -107,8 +177,4 @@ function highlight(color) {
 // ENABLE FIXED SCROLLING
 $(window).scroll(function () {
     $("#comments").css("top", Math.max(0, 250 - $(this).scrollTop()));
-});
-
-$("span").css({
-    "color": "green"
 });
